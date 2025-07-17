179acdf6beb08b1f3d3b7de8bd4523761dfd1d16
Jeff Ahn <ahn.jeff@gmail.com>
2025-07-16 18:59:40 -0700

    vertex parsing

---
+++ src/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants {
+
+const int nTriangles = 3;
+const int nVertices = 3 * nTriangles;
+
+};  // namespace constants
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,41 @@
 #define GLAD_GL_IMPLEMENTATION
 #include <glad/glad.h>
-
+////////////// line here for linter glad.h must be first
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include "constants.h"
+
+GLFWwindow* window;
+// Vertex buffer object
+unsigned int VBO;
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+void windowSetup() {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    exit(-1);
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return exit(-1);
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 }
 
 void processInput(GLFWwindow* window) {
@@ -13,34 +43,27 @@ void processInput(GLFWwindow* window) {
     glfwSetWindowShouldClose(window, true);
 }
 
+void vertexSetup(unsigned int& VBO, float vertices[], const int nVertices) {
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  std::cerr << "Size of vertices in bytes:" << nVertices * sizeof(vertices[0])
+            << std::endl;
+  glBufferData(GL_ARRAY_BUFFER, nVertices * sizeof(vertices[0]), vertices,
+               GL_STATIC_DRAW);
+}
+
 int main(void) {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  windowSetup();
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-  if (window == NULL) {
-    std::cout << "Failed to create GLFW window" << std::endl;
-    glfwTerminate();
-    return -1;
-  }
-  glfwMakeContextCurrent(window);
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    return -1;
-  }
-
-  glViewport(0, 0, 800, 600);
-
-  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+  float vertices[constants::nVertices] = {-.5, -.5, 0, .5, -.5, 0, 0, .5, 0};
+  vertexSetup(VBO, vertices, constants::nVertices);
 
   // Main rendering loop.
   while (!glfwWindowShouldClose(window)) {
+    // User input listener
     processInput(window);
 
-    // TODO: Rendering here
+    // Rendering
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
