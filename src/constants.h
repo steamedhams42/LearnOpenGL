1ad41e07c554703cd8e1876580c3f992c97eb4a3
Jeff Ahn <ahn.jeff@gmail.com>
2025-07-20 21:18:07 -0700

    add textures rendering

--- src/constants.h
+++ src/constants.h
@@ -3,6 +3,9 @@
 
 namespace constants {
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
 const int nTriangles = 3;
 const int nVertices = 3 * nTriangles;
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include "constants.h"
 #include "shader.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 GLFWwindow* window;
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
@@ -21,7 +24,8 @@ void windowSetup() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  window = glfwCreateWindow(constants::WIDTH, constants::HEIGHT, "LearnOpenGL",
+                            NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
@@ -34,7 +38,7 @@ void windowSetup() {
     return exit(-1);
   }
 
-  glViewport(0, 0, 800, 600);
+  glViewport(0, 0, constants::WIDTH, constants::HEIGHT);
 
   glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 }
@@ -121,11 +125,93 @@ void renderTriangle() {
 }
 
 void renderTexture() {
+  const char* vertex_shader_fp = "src/shaders/vertex/texture_vertex.vs";
+  const char* fragment_shader_fp = "src/shaders/fragment/texture.frag";
+  Shader texture_shader(vertex_shader_fp, fragment_shader_fp);
+
+  float vertices[] = {
+      // positions          // colors           // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3,  // first triangle
+      1, 2, 3   // second triangle
+  };
+  unsigned int VBO, VAO, EBO;
+  // Vertex buffer object
+  glGenBuffers(1, &VBO);
+  // Vertex array object
+  glGenVertexArrays(1, &VAO);
+  // Vertex buffer object
+  glGenBuffers(1, &EBO);
+
+  // (1) Bind the Vertex Array Object first
+  glBindVertexArray(VAO);
+
+  // (2a) Bind and copy vertices in vertex buffer object.
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // (2b) Bind vertex buffer
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // (3) Configure object position coordinates(s).
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // (4) Configure object color
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // (5) Configure texture coordinates
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  // Bind texture
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  // Texture wrapping options
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                  GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Load
+  int texture_width, texture_height, nChannels;
+  unsigned char* texture_data = stbi_load("src/container.jpg", &texture_width,
+                                          &texture_height, &nChannels, 0);
+  if (texture_data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width, texture_height, 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, texture_data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Texture data failed to load." << std::endl;
+    assert(false);
+  }
+  stbi_image_free(texture_data);
+
+  // Main rendering loop
   while (!glfwWindowShouldClose(window)) {
     // User input listener
     processInput(window);
 
+    // Bind texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
     // Rendering code here
+    texture_shader.use();
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     // Buffer swap
     glfwPollEvents();
