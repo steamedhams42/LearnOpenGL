232e4975afdddd6450de8ea4467fee8f9dc03045
Jeff Ahn <ahn.jeff@gmail.com>
2025-07-22 21:17:05 -0700

    get multiple rotating cubes. fix shader code bug

--- src/constants.h
+++ src/constants.h
@@ -8,6 +8,10 @@ const int HEIGHT = 600;
 
 const float ASPECT_RATIO = 1.0f * WIDTH / HEIGHT;
 
+// z-distance (depth) from frustums
+const float NEAR = 0.1f;
+const float FAR = 100.0f;
+
 const int nTriangles = 3;
 const int nVertices = 3 * nTriangles;
 
--- src/main.cpp
+++ src/main.cpp
@@ -57,75 +57,80 @@ void renderTexture() {
   Shader texture_shader(vertex_shader_fp, fragment_shader_fp);
 
   float vertices[] = {
-      // positions          // colors           // texture coords
-      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
-      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
-      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
-  };
-  unsigned int indices[] = {
-      0, 1, 3,  // first triangle
-      1, 2, 3   // second triangle
-  };
-  unsigned int VBO, VAO, EBO;
-  // Vertex buffer object
-  glGenBuffers(1, &VBO);
-  // Vertex array object
-  glGenVertexArrays(1, &VAO);
-  // Vertex buffer object
-  glGenBuffers(1, &EBO);
+      -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,
+      0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
 
-  // (1) Bind the Vertex Array Object first
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+      -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,
+
+      -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f,
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,
+
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+      -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 1.0f,
+      0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
+      -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+      -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
+      0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+      -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
+  // world space positions of our cubes
+  glm::vec3 cubePositions[] = {
+      glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
+      glm::vec3(-1.5f, -2.2f, -2.5f), glm::vec3(-3.8f, -2.0f, -12.3f),
+      glm::vec3(2.4f, -0.4f, -3.5f),  glm::vec3(-1.7f, 3.0f, -7.5f),
+      glm::vec3(1.3f, -2.0f, -2.5f),  glm::vec3(1.5f, 2.0f, -2.5f),
+      glm::vec3(1.5f, 0.2f, -1.5f),   glm::vec3(-1.3f, 1.0f, -1.5f)};
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
   glBindVertexArray(VAO);
 
-  // (2a) Bind and copy vertices in vertex buffer object.
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  // (2b) Bind vertex buffer
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-               GL_STATIC_DRAW);
-
-  // (3) Configure object position coordinates(s).
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-
-  // (4) Configure object color
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+  // texture coord attribute
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                         (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
-  // (5) Configure texture coordinates
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void*)(6 * sizeof(float)));
-  glEnableVertexAttribArray(2);
-
-  // Bind texture
+  // load and create a texture
+  // -------------------------
   unsigned int texture;
+  // texture 1
+  // ---------
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
-
-  // Texture wrapping options
+  // set the texture wrapping parameters
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
-                  GL_LINEAR_MIPMAP_LINEAR);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  // Load
-  int texture_width, texture_height, nChannels;
-  unsigned char* texture_data = stbi_load("src/container.jpg", &texture_width,
-                                          &texture_height, &nChannels, 0);
-  if (texture_data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texture_width, texture_height, 0,
-                 GL_RGB, GL_UNSIGNED_BYTE, texture_data);
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(
+      true);  // tell stb_image.h to flip loaded texture's on the y-axis.
+  unsigned char* data =
+      stbi_load("src/container.jpg", &width, &height, &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
   } else {
-    std::cout << "Texture data failed to load." << std::endl;
-    assert(false);
+    std::cout << "Failed to load texture" << std::endl;
   }
-  stbi_image_free(texture_data);
+  stbi_image_free(data);
 
   texture_shader.use();
   texture_shader.set_int("texture", 0);
@@ -135,7 +140,7 @@ void renderTexture() {
   // outside the main loop only once.
   glm::mat4 projection = glm::mat4(1.0f);
   projection = glm::perspective(glm::radians(45.0f), constants::ASPECT_RATIO,
-                                0.1f, 100.0f);
+                                constants::NEAR, constants::FAR);
   texture_shader.set_mat4("projection", projection);
 
   // Main rendering loop
@@ -143,30 +148,40 @@ void renderTexture() {
     // User input listener
     processInput(window);
 
-    // Clear background
-    glClear(GL_COLOR_BUFFER_BIT);
+    // Clear background and buffer bit
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Bind texture
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Activate shader
     texture_shader.use();
 
-    // Create matrix transformations
-    glm::mat4 model = glm::mat4(1.0f);
+    // Create transformations
     glm::mat4 view = glm::mat4(1.0f);
-    model =
-        glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    // Extract uniform matrix locations from shader
+    // retrieve the matrix uniform locations
     unsigned int modelLoc = glGetUniformLocation(texture_shader.id(), "model");
     unsigned int viewLoc = glGetUniformLocation(texture_shader.id(), "view");
-    // Pass them to the shaders (3 different ways)
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+    // pass them to the shaders (3 different ways)
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+    texture_shader.set_mat4("view", view);
 
-    // Rendering code here
+    // render box(es)
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    for (unsigned int i = 0; i < 10; i++) {
+      // calculate the model matrix for each object and pass it to shader before
+      // drawing
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f),
+                          glm::vec3(1.0f, 0.3f, 0.5f));
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      texture_shader.set_mat4("model", model);
+
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     // Buffer swap
     glfwPollEvents();
@@ -176,6 +191,7 @@ void renderTexture() {
 
 int main(void) {
   windowSetup();
+  glEnable(GL_DEPTH_TEST);
 
   int i = 1;
   switch (i) {
